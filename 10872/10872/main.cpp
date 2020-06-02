608cbf0308ff49998698cff34a6348d9f2b62915
Hong-Sen <hse025@naver.com>
2020-06-02 20:58:35 +0900

    10872

---
+++ 10872/10872/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int fac(int n){
+    if(n==0)return 1;
+    return n * fac(n-1);
+}
+
+int main(){
+    int N;
+    cin>>N;
+    cout<<fac(N)<<'\n';
+    
+}
