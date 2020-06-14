5acfeec6a0bce4312f7f7f0280dafa86388c1a0a
Hong-Sen <hse025@naver.com>
2020-06-14 23:01:34 +0900

    10773

---
+++ 10773/10773/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main(){
+    
+    int k;
+    cin>>k;
+    stack<int> st;
+    int sum = 0;
+    
+    for(int i=0;i<k;i++){
+        int n;
+        cin>>n;
+        
+        if(n==0){
+            st.pop();
+        }
+        else{
+            st.push(n);
+        }
+        
+    }
+    
+    
+    for(int i=0;!st.empty();i++){
+        sum+=st.top();
+        st.pop();
+    }
+    cout<<sum<<endl;
+    
+}
