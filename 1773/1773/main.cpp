b880bd187809a2d2b4c55e25580e58aac16b0634
Hong-Sen <hse025@naver.com>
2020-11-22 16:01:25 +0900

    1773

--- 1773/1773/main.cpp
+++ 1773/1773/main.cpp
@@ -1,23 +1,31 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
 bool isvisted[2000000];
+int arr[100];
 
 int main(){
     int n,t;
     cin>>n>>t;
     int sum = 0;
     for(int i=0; i<n; i++){
-        int x;
-        cin>>x;
-        int k = x;
+        cin>>arr[i];
+    }
+    
+    sort(arr,arr+n);
+    
+    for(int i=0; i<n; i++){
+        int x = arr[i];
+        if(isvisted[arr[i]])    continue; //이게 시간줄이는 포인트!!! 
         while(x <= t){
             if(!isvisted[x]){
                 sum ++;
                 isvisted[x] = true;
             }
-            x += k;
+            x += arr[i];
         }
     }
+    
     cout<<sum<<"\n";
 }
