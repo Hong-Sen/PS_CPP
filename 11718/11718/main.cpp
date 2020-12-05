91338d85d5a6e05093c81eadf80836d1e789ac4b
Hong-Sen <hse025@naver.com>
2020-12-05 15:29:19 +0900

    11718

---
+++ 11718/11718/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    while(1){
+        string s;
+        getline(cin,s);
+        if(s == "")  break;
+        cout<<s<<"\n";
+    }
+    return 0;
+}
