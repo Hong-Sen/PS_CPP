4673c0e65453ce3f9b89cc60c9b15996f8a0b1aa
Hong-Sen <hse025@naver.com>
2020-12-18 01:46:29 +0900

    Update main.cpp

--- 2490/2490/main.cpp
+++ 2490/2490/main.cpp
@@ -1,22 +1,12 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main(){
+    string s = "DCBAE";
     for(int i=0; i<3; i++){
-        int x,zero=0;
-        cin>>x;
-        if(x==0)    zero++;
-        cin>>x;
-        if(x==0)    zero++;
-        cin>>x;
-        if(x==0)    zero++;
-        cin>>x;
-        if(x==0)    zero++;
-        
-        if(zero==0) cout<<"E\n";
-        if(zero==1) cout<<"A\n";
-        if(zero==2) cout<<"B\n";
-        if(zero==3) cout<<"C\n";
-        if(zero==4) cout<<"D\n";
+        int a,b,c,d;
+        cin>>a>>b>>c>>d;
+        cout<<s.at(a+b+c+d)<<"\n";
     }
 }
