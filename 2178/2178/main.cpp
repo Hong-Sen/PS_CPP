e6e183731092ce4b098c9a9b4e4b77a76e6721d2
Hong-Sen <hse025@naver.com>
2021-04-26 18:02:59 +0900

    미로 탐색

--- 2178/2178/main.cpp
+++ 2178/2178/main.cpp
@@ -1,54 +1,42 @@
 #include <iostream>
-#include <string>
 #include <queue>
 using namespace std;
 
-int n,m;
-bool arr[101][101];
+int map[101][101];
 int visited[101][101];
-int dir[4][2] = {{1,0}, {-1,0}, {0,1}, {0,-1}};
+int n,m;
+int dx[4] = {-1,1,0,0};
+int dy[4] = {0,0,-1,1};
 
-bool isInside(int a, int b){
-    return (a>=0 && a<n) && (b>=0 && b<m);
-}
-
-int bfs(int x, int y){
-    visited[x][y] = 1;
+void bfs(int x,int y){
     queue<pair<int, int>> q;
-    q.push(pair<int, int>(x, y));
-    
+    q.push({x,y});
+    visited[x][y] = 1;
     while (!q.empty()) {
-        int curX = q.front().second;
-        int curY = q.front().first;
+        int curX = q.front().first;
+        int curY = q.front().second;
         q.pop();
-        
-        if((curY == n-1) && (curX == m-1))  break;
-        
+        if(curX == n-1 && curY == m-1)  break;
         for(int i=0; i<4; i++){
-            int nextX = curX + dir[i][1];
-            int nextY = curY + dir[i][0];
-            
-            if(isInside(nextY, nextX) && visited[nextY][nextX]==0 && arr[nextY][nextX]){
-                visited[nextY][nextX] = visited[curY][curX] + 1;
-                q.push(pair<int, int>(nextY, nextX));
+            int nx = curX + dx[i];
+            int ny = curY + dy[i];
+            if(nx>=0 && nx<n && ny>=0 && ny<m && map[nx][ny]==1 && visited[nx][ny]==0 ){
+                visited[nx][ny] = visited[curX][curY] + 1;
+                q.push({nx,ny});
             }
         }
-        
-        
     }
-   
-    return visited[n-1][m-1];
 }
+
 
 int main(){
     cin>>n>>m;
     for(int i=0; i<n; i++){
-        for(int j=0; j<m; j++){
-            int x;
-            scanf("%1d", &x);
-            if(x==1)    arr[i][j] = true;
-        }
+        string s;
+        cin>>s;
+        for(int j=0; j<m; j++)
+            map[i][j] = (int)s.at(j) - 48;
     }
-    cout<<bfs(0, 0)<<"\n";
-    
+    bfs(0, 0);
+    cout<<visited[n-1][m-1];
 }
