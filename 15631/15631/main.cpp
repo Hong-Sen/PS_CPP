eb54c20f002e4a9b89d69d49a678ffa699094e65
Hong-Sen <hse025@naver.com>
2021-05-31 16:08:43 +0900

    BOJeopardy

---
+++ 15631/15631/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout<<"newjersey\n"<<"herostratus\n"<<"seeyouagain\n"<<"aungsansuukyi\n"<<"7\n"
+    <<"1998\n"<<"tenere\n"<<"michaelcollins\n"<<"6\n"<<"dragster\n";
+}
