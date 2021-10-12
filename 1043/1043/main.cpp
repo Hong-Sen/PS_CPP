f795c5ac5cb438cb5ca2ce7d6347a187a9366620
Hong-Sen <20181053@sungshin.ac.kr>
2021-10-12 17:56:25 +0900

    거짓말

---
+++ 1043/1043/main.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int N,M;
+int truth,truthNum;
+int partyPeople,num;
+vector<int> know;
+vector<int> party[51];
+int parent[51];
+int cnt;
+
+int findParent(int a){
+    if(a == parent[a]) return a;
+    return parent[a] = findParent(parent[a]);
+}
+
+void unionParent(int a, int b){
+    a = findParent(a);
+    b = findParent(b);
+    parent[b] = a;
+}
+
+int main(){
+    cin>>N>>M;
+    cin>>truth;
+    for(int i=0; i<truth; i++){
+        cin>>truthNum;
+        know.push_back(truthNum);
+    }
+    for(int i=0; i<M; i++){
+        cin>>partyPeople;
+        for(int j=0; j<partyPeople; j++){
+            cin>>num;
+            party[i].push_back(num);
+        }
+    }
+    if(truth == 0){
+        cout<<M;
+        return 0;
+    }
+    // x의 부모 = x
+    for(int i=1; i<=N; i++)
+        parent[i] = i;
+    
+    // 파티 같이 하는 사람들 부모 하나로 합치기
+    for(int i=0; i<M; i++){
+        int p = party[i][0];
+        for(int j=1; j<party[i].size(); j++){
+            unionParent(p, party[i][j]);
+        }
+    }
+    
+    // 진실을 아는 사람들의 부모와 비교하기
+    for(int i=0; i<M; i++){
+        bool flag = true;
+        for(int k=0; k<party[i].size(); k++){
+            if(!flag) break;
+            for(int j=0; j<truth; j++){
+                if(findParent(party[i][k]) == findParent(know[j])){
+                    flag = false;
+                    break;
+                }
+            }
+        }
+        if(flag) cnt++;
+    }
+    cout<<cnt;
+}
