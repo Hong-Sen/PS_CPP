cacf221c982492d87bc420953c0d6fe379b141bc
Hong-Sen <20181053@sungshin.ac.kr>
2022-01-10 21:57:01 +0900

    1로 만들기 2

---
+++ 12852/12852/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int dp[1000001];
+int before[1000001];
+
+int main(){
+    int n;
+    cin>>n;
+    for(int i=2; i<=n; i++){
+        // 3번 연산(-1)
+        dp[i] = dp[i-1] + 1;
+        before[i] = i-1;
+        // 1번 연산(/3)
+        if(i%3==0 && dp[i] > dp[i/3]+1){
+            dp[i] = dp[i/3] + 1;
+            before[i] = i/3;
+        }
+        // 2번 연산(/2)
+        if(i%2==0 && dp[i] > dp[i/2] + 1){
+            dp[i] = dp[i/2] + 1;
+            before[i] = i/2;
+        }
+//        cout<<i<<": "<<dp[i]<<" "<<before[i]<<"\n";
+    }
+    cout<<dp[n]<<"\n";
+    while (1) {
+        cout<<n<<" ";
+        n = before[n];
+        if(n == 0) break;
+    }
+}
+
