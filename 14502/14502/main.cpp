ecb8f4f842efc82964e91120c7dba32bd204e6fe
Hong-Sen <20181053@sungshin.ac.kr>
2022-02-16 20:42:50 +0900

    연구소

--- 14502/14502/main.cpp
+++ 14502/14502/main.cpp
@@ -1,52 +1,69 @@
 #include <iostream>
 #include <vector>
 #include <queue>
-#include <algorithm>
 using namespace std;
 
 int n,m;
-int map[10][10];
-vector<pair<int, int>> v; // 0인 공간
-queue<pair<int, int>> q; // 2인 공간
+int map[9][9];
+int wall;
 int dx[4] = {-1,1,0,0};
 int dy[4] = {0,0,-1,1};
+queue<pair<int, int>> q;
 int result;
 
-int infection(vector<pair<int, int>> v){
-    queue<pair<int, int>> tq = q;
-    int arr[n][m];
-    
+bool isInside(int row, int col){
+    if(row>=0 && row<n && col>=0 && col<m) return true;
+    return false;
+}
+
+int infection(int tmp[][9]){
+    queue<pair<int, int>> q2;
+    int cnt = 0;
+//
+    q2 = q;
     for(int i=0; i<n; i++){
         for(int j=0; j<m; j++){
-            arr[i][j] = map[i][j];
+            if(tmp[i][j] == 2)  q2.push({i,j});
+            cout<<tmp[i][j]<<" ";
         }
+        cout<<"\n";
     }
-    // 3개 벽으로 막기
-    for(int i=0; i<3; i++){
-        arr[v[i].first][v[i].second] = 1;
-    }
-    
-    while (!tq.empty()) {
-        int y = tq.front().first;
-        int x = tq.front().second;
-        tq.pop();
+    while (!q2.empty()) {
+        int cr = q2.front().first;
+        int cc = q2.front().second;
+        cout<<cr<<" "<<cc<<"\n";
+        cnt++;
+        q2.pop();
         for(int i=0; i<4; i++){
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-            if(nx>=0 && ny>=0 && nx<m && ny<n && arr[ny][nx] == 0){
-                arr[ny][nx] = 2;
-                tq.push({ny,nx});
+            int nr = cr + dy[i];
+            int nc = cc + dx[i];
+            if(isInside(nr, nc) && tmp[nr][cc] == 0){
+                q2.push({nr, nc});
+                tmp[nr][nc] = 2;
             }
         }
     }
-    //find safe place
-    int cnt = 0;
+    return n * m - wall - cnt;
+}
+
+void makeWall(int tmp[][9], int cnt){
+    if(cnt == 3){
+        cout<<"cnt3\n";
+        result = max(result, infection(tmp));
+        cout<<"result: "<<result<<"\n";
+    }
+    
+    int (*map2)[9] = tmp;
     for(int i=0; i<n; i++){
+        cout<<"i: "<<i<<"\n";
         for(int j=0; j<m; j++){
-            if(arr[i][j] == 0)  cnt++;
+            if(map2[i][j] == 0){
+                map2[i][j] = 1;
+                makeWall(map2, cnt+1);
+                map2[i][j] = 0;
+            }
         }
     }
-    return cnt;
 }
 
 int main(){
@@ -54,23 +71,11 @@ int main(){
     for(int i=0; i<n; i++){
         for(int j=0; j<m; j++){
             cin>>map[i][j];
-            if(map[i][j] == 0) v.push_back({i,j});
             if(map[i][j] == 2) q.push({i,j});
+            if(map[i][j] == 1) wall++;
         }
     }
-    
-    int vSize = v.size();
-    // 3개읩 벽 선정
-    for (int i=0; i<vSize-2; i++) {
-        for(int j=i+1; j<vSize-1; j++){
-            for(int l=j+1; l<vSize; l++){
-                vector<pair<int, int>> tmp;
-                tmp.push_back({v[i].first,v[i].second});
-                tmp.push_back({v[j].first,v[j].second});
-                tmp.push_back({v[l].first,v[l].second});
-                result = max(infection(tmp),result);
-            }
-        }
-    }
+    makeWall(map, 0);
     cout<<result;
+
 }
