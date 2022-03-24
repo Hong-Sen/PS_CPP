80ae201bf8b391db0a386df026c64c7bf2d7e1a5
Hong-Sen <20181053@sungshin.ac.kr>
2022-03-24 23:45:46 +0900

    fix 뱀

--- 3190/3190/main.cpp
+++ 3190/3190/main.cpp
@@ -1,109 +1,15 @@
-//
-//  main.cpp
-//  3190
-//
-//  Created by 홍세은 on 2022/03/23.
-//
-
 #include <iostream>
 #include <queue>
 using namespace std;
 
 int N,K,L;
 int map[101][101];
-queue<pair<int, char>> q;
-int endTime;
+queue<pair<int, char>> spin;
+queue<pair<int, int>> q;
+int direction[4][2] = {{0,1}, {-1,0}, {0,-1}, {1,0}}; // {E,N,W,S}
 
 bool isInside(int r, int c){
     return r>=0 && r<N && c>=0 && c<N;
-}
-
-void dfs(int curLocation[2], queue<pair<int, int>> snakeOrder, int time, char d, int snake[101][101]){
-    // 종료 조건: 벽 또는 자기자신과 부딫힘
-    if(!isInside(curLocation[0], curLocation[1]) || snake[curLocation[0]][curLocation[1]] == 1){
-        endTime = time;
-        cout<<endTime<<"\n";
-        return;
-    }
-    
-    // 머리 이동
-    snake[curLocation[0]][curLocation[1]] = 1;
-    snakeOrder.push({curLocation[0], curLocation[1]});
-    
-    // 사과x
-    if(time != 0 && map[curLocation[0]][curLocation[1]] == 0){
-        // 꼬리 이동
-        snake[snakeOrder.front().first][snakeOrder.front().second] = 0;
-        snakeOrder.pop();
-    }
-    // 사과 먹었으면 없애기!
-    else if(map[curLocation[0]][curLocation[1]] == 1) {
-        map[curLocation[0]][curLocation[1]] = 0;
-    }
-    
-    char nextD = d;
-    if(q.front().first == time) {
-        int spinDirection = q.front().second;
-        q.pop();
-        if(spinDirection == 'D') {
-            switch (d) {
-                case 'E':
-                    nextD = 'S';
-                    break;
-                case 'W':
-                    nextD = 'N';
-                    break;
-                case 'N':
-                    nextD = 'E';
-                    break;
-                case 'S':
-                    nextD = 'W';
-                    break;
-                default:
-                    break;
-            }
-        }
-        if(spinDirection == 'L') {
-            switch (d) {
-                case 'E':
-                    nextD = 'N';
-                    break;
-                case 'W':
-                    nextD = 'S';
-                    break;
-                case 'N':
-                    nextD = 'W';
-                    break;
-                case 'S':
-                    nextD = 'E';
-                    break;
-                default:
-                    break;
-            }
-        }
-    }
-    
-    switch (nextD) {
-        case 'E':
-            curLocation[1] += 1;
-            dfs(curLocation, snakeOrder, time+1, nextD, snake);
-            break;
-        case 'W':
-            curLocation[1] -= 1;
-            dfs(curLocation, snakeOrder,time+1, nextD, snake);
-            break;
-        case 'N':
-            curLocation[0] -= 1;
-            dfs(curLocation, snakeOrder, time+1, nextD, snake);
-            break;
-        case 'S':
-            curLocation[0] += 1;
-            dfs(curLocation, snakeOrder, time+1, nextD, snake);
-            break;
-            
-        default:
-            break;
-    }
 }
 
 int main(){
@@ -115,13 +21,50 @@ int main(){
     }
     cin>>L;
     int t;
-    char c;
+    char spinDir;
     for(int i=0; i<L; i++){
-        cin>>t>>c;
-        q.push({t,c});
+        cin>>t>>spinDir;
+        spin.push({t,spinDir});
     }
+    
+    int now = 0, r = 0, c = 0, dir = 0;
     int snake[101][101] = {0,};
-    int curLocation[2] = {0,0};
-    queue<pair<int, int>> snakeOrder;
-    dfs(curLocation, snakeOrder, 0, 'E', snake);
+    q.push({0,0});
+    while (1) {
+        // 종료 조건
+        if(!isInside(r, c) || snake[r][c] == 1) break;
+        // 사과 X -> 꼬리 이동
+        if(map[r][c] == 0){
+            snake[q.front().first][q.front().second] = 0;
+            q.pop();
+        }
+        // 머리 이동
+        snake[r][c] = 1;
+        q.push({r,c});
+        
+//        cout<<"\n";
+//        for(int i=0; i<N; i++){
+//            for(int j=0; j<N; j++){
+//                cout<<snake[i][j]<<" ";
+//            }
+//            cout<<"\n";
+//        }
+        
+        // 전진
+        r += direction[dir][0];
+        c += direction[dir][1];
+        now++;
+        
+        if(now == spin.front().first){
+            cout<<now<<" "<<spin.front().second<<"\n";
+            if(spin.front().second == 'L') {
+                dir = (dir+1) % 4;
+            }
+            else {
+                dir = dir - 1 < 0 ? 3 : dir - 1;
+            }
+            spin.pop();
+        }
+    }
+    cout<<now;
 }
