1cccd63633603ddfe63f85b26d254e7071cffc90
Hong-Sen <20181053@sungshin.ac.kr>
2022-03-29 23:50:38 +0900

    fix 구명보트

--- Programmers/구명보트/구명보트/main.cpp
+++ Programmers/구명보트/구명보트/main.cpp
@@ -5,6 +5,10 @@
 #include <iostream>
 using namespace std;
 
+/* 맨 앞사람 + 맨 뒷사람 > limit 인 경우
+   맨 뒷사람은 어느 누구와 같이 탈 수 없음
+   맨 뒷사람은 바로 보내기 */
+
 int solution(vector<int> people, int limit) {
     int answer = 0;
     deque<int> dq;
@@ -27,4 +31,8 @@ int solution(vector<int> people, int limit) {
         }
     }
     return answer;
+}
+
+int main(){
+   cout<<solution({70, 50, 80, 50} ,100);
 }
