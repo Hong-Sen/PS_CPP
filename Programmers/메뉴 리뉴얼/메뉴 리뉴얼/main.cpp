512a311dc0d80bc2d7c78bbb37d781f8cab464ed
Hong-Sen <20181053@sungshin.ac.kr>
2022-06-04 01:16:51 +0900

    [fix] 메뉴 리뉴얼

--- Programmers/메뉴 리뉴얼/메뉴 리뉴얼/main.cpp
+++ Programmers/메뉴 리뉴얼/메뉴 리뉴얼/main.cpp
@@ -2,33 +2,19 @@
 #include <vector>
 #include <map>
 #include <algorithm>
-#include <iostream>
 using namespace std;
 
- map<string, int> m;
- vector<pair<int, string>> v[11];
- vector<int> courseList;
-
-bool isIncludeCourse(string s){
-    int n = s.size();
-    
-    for(int i: courseList) {
-        if(n == i) return true;
-    }
-    
-    return false;
-}
+map<string, int> m;
+vector<pair<int, string>> v[12];
 
 void makeSubset(int idx, string s, string origin){
     if(s.size() >= 2){
-        if(isIncludeCourse(s)) {
-           if(m.find(s) != m.end()){
+        if(m.find(s) != m.end()){
             m[s]++;
         }
         else {
-             m.insert({s, 2});
+             m.insert({s, 1});
         }
-      }
     }
     
     for(int i=idx; i<origin.size(); i++){
@@ -37,32 +23,19 @@ void makeSubset(int idx, string s, string origin){
 }
 
 vector<string> solution(vector<string> orders, vector<int> course) {
-    courseList = course;
     vector<string> answer;
     
-    for(string s: orders){
+    for(string s: orders) {
         sort(s.begin(), s.end());
+        makeSubset(0, "", s);
     }
     
-    for(int i=0; i<orders.size()-1; i++){
-        for(int j=i+1; j<orders.size(); j++) {
-            string s = "";
-            
-            for(int k=0; k<orders[j].size(); k++){
-                char c = orders[j][k];
-                if(orders[i].find(c) <= orders[i].size()) s += c;
-            }
-            
-            makeSubset(0, "", s);
-            
-        }
-    }
-    
-    for(auto it = m.begin(); it != m.end(); it++) {
+    for(auto it = m.begin(); it != m.end(); it++){
+        if(it->second == 1) continue;
         v[(it->first).size()].push_back({it->second, it->first});
     }
     
-    for(int i=2; i<11; i++){
+    for(int i: course){
         sort(v[i].begin(), v[i].end(), greater<>());
         int maxMenu = v[i][0].first;
         for(int j=0; j<v[i].size(); j++){
