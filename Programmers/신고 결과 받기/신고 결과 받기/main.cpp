66ba3a86867c11af9c6c04ccf7a9772d0b1f82a2
Hong-Sen <20181053@sungshin.ac.kr>
2022-06-06 20:43:54 +0900

    [fix] 신고 결과 받기

--- Programmers/신고 결과 받기/신고 결과 받기/main.cpp
+++ Programmers/신고 결과 받기/신고 결과 받기/main.cpp
@@ -1,15 +1,16 @@
 #include <string>
 #include <vector>
 #include <map>
+#include <set>
 #include <iostream>
 using namespace std;
 
-int reportedCnt[1001];
+int calledCnt[1001];
 
 vector<int> solution(vector<string> id_list, vector<string> report, int k) {
     vector<int> answer;
     map<string, int> user;
-    map<int, int> reporting[1001];
+    map<int, int> caller[1001];
     
     for(int i=0; i<id_list.size(); i++) {
         user.insert({id_list[i], i});
@@ -26,19 +27,19 @@ vector<int> solution(vector<string> id_list, vector<string> report, int k) {
             }
             tmp += s[i];
         }
-        reporting[userId].insert({user[tmp], 0});
+        caller[userId].insert({user[tmp], 0});
     }
     
     for(int i=0; i<id_list.size(); i++) {
-        for(auto it = reporting[i].begin(); it != reporting[i].end(); it++){
-            reportedCnt[it->first]++;
+        for(auto it = caller[i].begin(); it != caller[i].end(); it++){
+            calledCnt[it->first]++;
         }
     }
     
     for(int i=0; i<id_list.size(); i++) {
         int cnt = 0;
-       for(auto it = reporting[i].begin(); it != reporting[i].end(); it++) {
-            if(reportedCnt[it->first] >= k) cnt++;
+       for(auto it = caller[i].begin(); it != caller[i].end(); it++) {
+            if(calledCnt[it->first] >= k) cnt++;
         }
         answer.push_back(cnt);
     }
