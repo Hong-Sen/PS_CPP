04f9f1477a7b20d6b10c6981255d3d4a2c57050b
Hong-Sen <20181053@sungshin.ac.kr>
2022-06-13 13:50:54 +0900

    [fix] 124 나라의 숫자

--- Programmers/124 나라의 숫자/124 나라의 숫자/main.cpp
+++ Programmers/124 나라의 숫자/124 나라의 숫자/main.cpp
@@ -6,24 +6,11 @@ using namespace std;
 
 string solution(int n) {
     string answer = "";
-    char nums[3] = {'1','2','4'};
-    int len = 0;
     while(n > 0) {
-        len++;
-        n -= pow(3,len);
-    }
-    
-    n += pow(3,len) - 1;
-    
-    while(1) {
-        len--;
-        if(len == 0) {
-            answer += nums[n];
-            break;
-        }
-        int dn = n/pow(3,len);
-        n = n % int(pow(3,len));
-        answer += nums[dn];
+        n--;
+        int rest = n % 3;
+        answer = "124"[rest] + answer;
+        n /= 3;
     }
     return answer;
 }
