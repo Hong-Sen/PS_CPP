3185492e775992561e353bb59d7b2b9b272fa39b
Hong-Sen <20181053@sungshin.ac.kr>
2022-06-16 23:01:18 +0900

    예산

---
+++ Programmers/예산/예산/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int solution(vector<int> d, int budget) {
+    int answer = 0;
+    sort(d.begin(), d.end());
+    long long sum = 0;
+    for(int i: d) {
+        sum += i;
+        if(sum > budget) break;
+        answer++;
+    }
+    return answer;
+}
