aecdb7ad4edfd89db4242c22bc52b2230c8adc4d
Hong-Sen <20181053@sungshin.ac.kr>
2022-06-17 23:39:45 +0900

    가운데 글자 가져오기

---
+++ Programmers/가운데 글자 가져오기/가운데 글자 가져오기/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  가운데 글자 가져오기
+//
+//  Created by 홍세은 on 2022/06/17.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
