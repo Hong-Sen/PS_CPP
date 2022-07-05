a18d15f1a61b88706baa083d7ecb2810a47d0ad4
Hong-Sen <20181053@sungshin.ac.kr>
2022-07-05 16:55:45 +0900

    서강그라운드
    플로이드 와샬 이용

--- 14938/14938/main.cpp
+++ 14938/14938/main.cpp
@@ -1,61 +1,56 @@
+//
+//  main.cpp
+//  14938
+//
+//  Created by 홍세은 on 2022/07/05.
+//
+
 #include <iostream>
 #include <vector>
-#include <queue>
-#include <cstring>
 #define INF 987654321
 using namespace std;
 
-int n,m,r,t,a,b,l;
+int n,m,r;
 int item[101];
-vector<pair<int, int>> v[101];
-int dist[101];
-bool visited[101];
+int adj[101][101];
+int dis[101][101];
 int result;
-
-void resetDist(){
-    for(int i=1; i<=n; i++)
-        dist[i] = INF;
-}
-void collect(int start){
-    resetDist();
-    memset(visited, false, sizeof(visited));
-    queue<pair<int, int>> q;
-    q.push({start, 0});
-    dist[start] = 0;
-    visited[start] = true;
-    while (!q.empty()) {
-        int cur = q.front().first;
-        int cost = q.front().second;
-        q.pop();
-        for(int i=0; i<v[cur].size(); i++){
-            int next = v[cur][i].first;
-            int nextCost = v[cur][i].second + cost;
-            if(dist[next] < nextCost)   continue;
-            if(nextCost <= m){
-                dist[next] = nextCost;
-                q.push({next, nextCost});
-                visited[next] = true;
-            }
-        }
-    }
-}
 
 int main(){
     cin>>n>>m>>r;
-    for(int i=1; i<=n; i++){
-        cin>>t;
-        item[i] = t;
+    for(int i=0; i<n; i++) {
+        cin>>item[i];
     }
-    for(int i=0; i<r; i++){
+    
+    int a,b,l;
+    for(int i=0; i<r; i++) {
         cin>>a>>b>>l;
-        v[a].push_back({b,l});
-        v[b].push_back({a,l});
+        adj[a-1][b-1] = l;
+        adj[b-1][a-1] = l;
     }
-    for(int i=1; i<=n; i++){
-        collect(i);
+    
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<n; j++) {
+            if(adj[i][j] == 0) adj[i][j] = INF;
+            if(i==j) adj[i][j] = 0;
+        }
+    }
+    
+    // 각 정점끼리 최단 거리 구하기
+    for(int k=0; k<n; k++) {
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<n; j++) {
+                adj[i][j] = min(adj[i][j], adj[i][k] + adj[k][j]);
+            }
+        }
+    }
+    
+    // 최대 아이템수 구하기
+    for(int i=0; i<n; i++) {
         int sum = 0;
-        for(int i=1; i<=n; i++){
-            if(visited[i])  sum += item[i];
+        for(int j=0; j<n; j++) {
+            if(adj[i][j] <= m) sum += item[j];
+
         }
         result = max(result, sum);
     }
