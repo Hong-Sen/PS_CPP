f66a26f3aae038dcb96daa9def3b6424854881ad
Hong-Sen <20181053@sungshin.ac.kr>
2023-05-13 00:09:01 +0900

    [FIX] 보석쇼핑
    start,end 모두 0에서 출발하여 조건에 맞게 증가시킴

--- Programmers/보석 쇼핑/보석 쇼핑/main.cpp
+++ Programmers/보석 쇼핑/보석 쇼핑/main.cpp
@@ -1,46 +1,41 @@
-// 55:00
 #include <string>
 #include <vector>
 #include <map>
 #include <iostream>
 using namespace std;
 
-map<string, int> m;
-int total;
-int totalSize = 100001;
+map<string,int> mp;
 
 vector<int> solution(vector<string> gems) {
     vector<int> answer;
-    int s = 0, e = 0;
+    int cnt = 100001;
+    
     for(string s: gems) {
-        m.insert({s,1});
-    }
-    total = m.size();
-    m.clear();
-    for(string s: gems) {
-        m[s]++;
-        if(m.size() == total) break;
-        e++;
+        mp[s]++;
     }
     
-    while(1) {
-        if(e - s + 1 < totalSize) {
-            totalSize = e - s + 1;
+    int kind = mp.size();
+    int s = 0, e = 0;
+    map<string,int> buy;
+    
+    while(s <= e) {
+        if(e >= gems.size() && buy.size() < kind) break;
+        
+        if(buy.size() >= kind) {
+            if(buy[gems[s]] == 1) buy.erase(gems[s]);
+            else buy[gems[s]]--;
+            s++;
+        }
+        else {
+            buy[gems[e++]]++;
+        }
+        
+        if(buy.size() == kind && cnt > (e-s)) {
+            cnt = e - s;
             answer.clear();
             answer.push_back(s+1);
-            answer.push_back(e+1);
+            answer.push_back(e);
         }
-        string key = gems[s];
-        m[key]--;
-        s++;
-        if(m[key] == 0) {
-            e++;
-            for(; e<gems.size(); e++) {
-                m[gems[e]]++;
-                if(gems[e] == key) break;
-            }
-        }
-        if(e >= gems.size()) break;
     }
     
     return answer;
